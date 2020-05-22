f0b3bbdc2b0e3c8f438371e80051de4d0b1f9dd6
Yuran <ybeliaev@gmail.com>
2020-05-22 19:21:14 +0300

    'tests'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, Yura!</h1>
+      <Person />
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const person = () => {
+  return <h2>Hi, person</h2>;
+};
+
+export default person;
