d506d3fd0ceca5b796deacf64872c606bd1d06aa
Yuran <ybeliaev@gmail.com>
2020-05-26 11:42:45 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import "./App.css";
 import Person from "./Person/Person";
 
 function App() {
+  function getAge() {
+    return Math.floor(Math.random() * 50);
+  }
   return (
     <div className="App">
-      <h1>Hello, Yura!</h1>
-      <Person />
+      <h1>Hello!</h1>
+
+      <Person name="John" age={getAge()} />
+      <Person name="Maxim" age={getAge()} />
+      <Person name="Yulia" age={getAge()}>
+        My Hobbies - racing
+      </Person>
     </div>
   );
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const person = () => {
-  return <h2>Hi, person</h2>;
+const person = (props) => {
+  return (
+    <div>
+      <h3>
+        Hi, {props.name}! You have {props.age} yeare
+      </h3>
+      <p>{props.children}</p>
+      {console.log(props)}
+    </div>
+  );
 };
 
 export default person;
