6f9f3edf3ca1f6f3298204f98c09771b4f34c538
Yuran <ybeliaev@gmail.com>
2020-05-26 16:28:49 +0300

    rename

---
+++ src/App-class.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Julia", age: 26 },
+      { name: "Max", age: 32 },
+      { name: "Nikolya", age: 41 },
+    ],
+    otherState: "some other value",
+  };
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Marry", age: 33 },
+        { name: "Foxy", age: 17 },
+        { name: "Nino", age: 32 },
+      ],
+    });
+  };
+
+  getAge = () => {
+    return Math.floor(Math.random() * 50);
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello!</h1>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.getAge()} />
+        <Person name={this.state.persons[1].name} age={this.getAge()} />
+        <Person name={this.state.persons[2].name} age={this.getAge()}>
+          My Hobbies - racing
+        </Person>
+      </div>
+    );
+  }
+}
+
+export default App;
