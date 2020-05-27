226714b847be02f8e2e1eb0bf6ac30e50944fc31
Yuran <ybeliaev@gmail.com>
2020-05-27 11:40:25 +0300

    add css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <h3 onClick={props.click}>
         Hi, {props.name}! You have {props.age} yeare
       </h3>
